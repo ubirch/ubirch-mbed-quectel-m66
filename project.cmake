89182ec288524b7b052c69ceaaad0f1ff6147972
Matthias L. Jugel <leo@thinkberg.com>
2018-11-06 17:57:21 +0100

    add missing project.cmake

---
+++ project.cmake
@@ -0,0 +1,2 @@
+add_executable(test-modem TESTS/m66network/modem/main.cpp TESTS/m66network/modem/config.h)
+add_executable(test-timestamp TESTS/m66network/unixTimestamp/unixTimestamp.cpp TESTS/m66network/unixTimestamp/config.h)
